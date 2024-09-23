e1412285ed44f81019b7f64efd946d5b089f1e75
Muhammad Fawwaz EFS <faw.edsa@gmail.com>
2024-09-23 16:31:23 +0700

    tugas 4

--- bemostore/settings.py
+++ bemostore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8032%y)@ko5r2y2+w0bi#(^bb1gc7)kd7of)2cl@m%x3a59*^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-fawwaz35-bemostore.pbp.cs.ui.ac.id"]
 
